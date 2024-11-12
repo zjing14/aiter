181e82ef7d86b3f0e65d02f4dda09585fbd3ce38
Marvin Tsai <marvin.tsai@a_m_d.c_o_m>
2024-11-12 09:46:27 +0000

    fuse a8w8 gemm with bias

--- aterKernels/gemm_op_a8w8.py
+++ aterKernels/gemm_op_a8w8.py
@@ -1,16 +1,17 @@
 import torch
 from aterKernels import gemm_a8w8
 
-def gemm_a8w8_(
+def gemm_a8w8_bias(
         XQ: torch.tensor,
         WQ: torch.tensor,
         x_scale: torch.tensor,
         w_scale: torch.tensor,
+        bias=None,
         dtype=torch.bfloat16):
     assert dtype in [torch.bfloat16, torch.float16], \
         f"Output {dtype=} is currently not supported in gemm_a8w8"
     m = XQ.shape[0]
     n = WQ.shape[0]
-    Y = torch.empty(m, n, dtype=dtype).cuda()
-    gemm_a8w8(XQ, WQ, x_scale, w_scale, Y)
+    Y = torch.empty(m, n, dtype=dtype, device="cuda")
+    gemm_a8w8(XQ, WQ, x_scale, w_scale, Y, bias)
     return Y
---
+++ op_tests/test_gemm_a8w8.py
@@ -0,0 +1,48 @@
+from test_common import checkAllclose, perftest
+import torch
+import torch.nn.functional as F
+import numpy as np
+import sys
+import os
+from aterKernels import gemm_op_a8w8
+
+
+@perftest()
+def run_torch(x, weight, x_scale, w_scale, bias=None, dtype=torch.bfloat16):
+    x = F.linear(x.to(torch.float32), weight.to(torch.float32))
+    scale = torch.matmul(x_scale, w_scale)
+    out = torch.mul(x, scale)
+    if bias is not None:
+        out = out.to(bias) + bias
+    return out.to(dtype)
+
+
+@perftest()
+def run_gemm_b(x, weight, x_scale, w_scale, bias=None, dtype=torch.bfloat16):
+    return gemm_op_a8w8.gemm_a8w8_bias(x, weight, x_scale, w_scale, bias, dtype = dtype)
+
+
+def test_gemm(dtype, m, n, k):
+    dim = (m, n, k)
+    x = torch.randint(-20, 20, (m, k), dtype=torch.int8).cuda()
+    weight = torch.randint(-20, 20, (n, k), dtype=torch.int8).cuda()
+    x_scale = torch.rand([m, 1], dtype=torch.float32).cuda()
+    w_scale = torch.rand([1, n], dtype=torch.float32).cuda()
+    bias = torch.rand([1,n],dtype = dtype).cuda()
+    (a, *_), avg_a = run_torch(x, weight, x_scale, w_scale, bias, dtype)
+    (b, *_), avg_b = run_gemm_b(x, weight, x_scale, w_scale, bias, dtype)
+
+    msg = f"[perf] dim: {str(dim):<20} dtype: {dtype}, torch avg: {avg_a:<8.2f} us, B avg: {avg_b:<8.2f} us, uplift: {avg_a/avg_b-1:<5.1%}"
+    checkAllclose(a, b, msg=msg, rtol=1e-3, atol=1000)
+
+
+for dtype in [torch.bfloat16]:
+    # qkv_proj
+    for (m, n, k) in [(4096, 1280, 8192),
+                      (128, 1280, 8192)
+                      ]:
+        test_gemm(dtype, m, n, k)
+    # attn_out
+    for (m, n, k) in [(4096, 8192, 1024),
+                      (128, 8192, 1024)]:
+        test_gemm(dtype, m, n, k)
