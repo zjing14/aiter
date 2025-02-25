0a55878bf8341167fd989e615afe611907cd164b
rahulbatra85 <rahulbatra85@gmail.com>
2025-02-25 10:48:23 -0600

    Add MoE align block size Triton Kernel (#113)
    Co-authored-by: Rahul Batra <rahbatra@amd.com>

---
+++ aiter/ops/triton/moe_align_block_size.py
@@ -0,0 +1,151 @@
+import pytest
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+import triton
+import triton.language as tl
+
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+@triton.jit
+def moe_align_block_size_stage1(
+    topk_ids_ptr,
+    tokens_cnts_ptr,
+    num_experts: tl.constexpr,
+    numel: tl.constexpr,
+    tokens_per_thread: tl.constexpr,
+):
+    pid = tl.program_id(0)
+
+    start_idx = pid * tokens_per_thread
+
+    off_c = (pid + 1) * num_experts
+
+    for i in range(tokens_per_thread):
+        if start_idx + i < numel:
+            idx = tl.load(topk_ids_ptr + start_idx + i)
+            token_cnt = tl.load(tokens_cnts_ptr + off_c + idx)
+            tl.store(tokens_cnts_ptr + off_c + idx, token_cnt + 1)
+
+
+@triton.jit
+def moe_align_block_size_stage2(
+    tokens_cnts_ptr,
+    num_experts: tl.constexpr,
+):
+    pid = tl.program_id(0)
+
+    last_cnt = 0
+    for i in range(1, num_experts + 1):
+        token_cnt = tl.load(tokens_cnts_ptr + i * num_experts + pid)
+        last_cnt = last_cnt + token_cnt
+        tl.store(tokens_cnts_ptr + i * num_experts + pid, last_cnt)
+
+
+@triton.jit
+def moe_align_block_size_stage3(
+    total_tokens_post_pad_ptr,
+    tokens_cnts_ptr,
+    cumsum_ptr,
+    num_experts: tl.constexpr,
+    block_size: tl.constexpr,
+):
+    last_cumsum = 0
+    off_cnt = num_experts * num_experts
+    for i in range(1, num_experts + 1):
+        token_cnt = tl.load(tokens_cnts_ptr + off_cnt + i - 1)
+        last_cumsum = last_cumsum + tl.cdiv(token_cnt, block_size) * block_size
+        tl.store(cumsum_ptr + i, last_cumsum)
+    tl.store(total_tokens_post_pad_ptr, last_cumsum)
+
+
+@triton.jit
+def moe_align_block_size_stage4(
+    topk_ids_ptr,
+    sorted_token_ids_ptr,
+    expert_ids_ptr,
+    tokens_cnts_ptr,
+    cumsum_ptr,
+    num_experts: tl.constexpr,
+    block_size: tl.constexpr,
+    numel: tl.constexpr,
+    tokens_per_thread: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    start_idx = tl.load(cumsum_ptr + pid)
+    end_idx = tl.load(cumsum_ptr + pid + 1)
+
+    for i in range(start_idx, end_idx, block_size):
+        tl.store(expert_ids_ptr + i // block_size, pid)
+
+    start_idx = pid * tokens_per_thread
+    off_t = pid * num_experts
+
+    for i in range(start_idx, tl.minimum(start_idx + tokens_per_thread,
+                                         numel)):
+        expert_id = tl.load(topk_ids_ptr + i)
+        token_cnt = tl.load(tokens_cnts_ptr + off_t + expert_id)
+        rank_post_pad = token_cnt + tl.load(cumsum_ptr + expert_id)
+        tl.store(sorted_token_ids_ptr + rank_post_pad, i)
+        tl.store(tokens_cnts_ptr + off_t + expert_id, token_cnt + 1)
+
+
+
+def moe_align_block_size_triton(
+    topk_ids: torch.Tensor, #[num_tkns, num_experts]
+    num_experts: int,
+    block_size: int,
+    sorted_token_ids: torch.Tensor,
+    expert_ids: torch.Tensor,
+    num_tokens_post_pad: torch.Tensor,
+) -> None:
+    numel = topk_ids.numel()
+    grid = (num_experts, )
+    tokens_cnts = torch.zeros((num_experts + 1, num_experts),
+                                dtype=torch.int32,
+                                device=topk_ids.device)
+    cumsum = torch.zeros((num_experts + 1,),
+                            dtype=torch.int32,
+                            device=topk_ids.device)
+    tokens_per_thread = ceil_div(numel, num_experts)
+
+    moe_align_block_size_stage1[grid](
+        topk_ids,
+        tokens_cnts,
+        num_experts,
+        numel,
+        tokens_per_thread,
+    )
+    #print(f"Post Stage1: topk_ids={topk_ids}")
+    #print(f"token_cnts={tokens_cnts}")
+    #print(f"num_experts={num_experts}, numel={numel}, tokens_per_thread={tokens_per_thread}")
+
+    moe_align_block_size_stage2[grid](
+        tokens_cnts,
+        num_experts,
+    )
+    #print(f"Post Stage2: token_cnts={tokens_cnts}")
+    moe_align_block_size_stage3[(1, )](
+        num_tokens_post_pad,
+        tokens_cnts,
+        cumsum,
+        num_experts,
+        block_size,
+    )
+    #print(f"Post Stage3: token_cnts={tokens_cnts}")
+    #print(f"Post Stage3: cumsum={cumsum}")
+    moe_align_block_size_stage4[grid](
+        topk_ids,
+        sorted_token_ids,
+        expert_ids,
+        tokens_cnts,
+        cumsum,
+        num_experts,
+        block_size,
+        numel,
+        tokens_per_thread,
+    )
+    #print(f"Post Stage4: token_cnts={tokens_cnts}")
+    #print(f"Post Stage4: cumsum={cumsum}")
---
+++ op_tests/triton/test_moe_align_block_size.py
@@ -0,0 +1,136 @@
+import triton
+import torch
+import triton.language as tl
+import pytest
+from typing import Any, Dict, Optional, Tuple
+import os
+import json
+import functools
+import argparse
+import sys
+
+from aiter.ops.triton.moe_align_block_size import moe_align_block_size_triton
+
+def _torch_moe_align_block_size(topk_ids: torch.Tensor, num_experts: int, top_k: int, block_size: int,
+                          sorted_token_ids: torch.Tensor, expert_ids: torch.Tensor,
+                          num_tokens_post_pad: torch.Tensor) -> None:
+    M, top_k = topk_ids.shape
+
+    expert_to_tokens = [[] for _ in range(num_experts)]
+    # For each token, for each selected expert, we append (token_id, expert)
+    for token_id in range(M):
+        for j in range(top_k):
+            e_id = topk_ids[token_id, j].item()
+            expert_to_tokens[e_id].append(token_id * top_k + j)
+
+    # Reorder tokens block by block, padding if needed
+    reordered_token_ids = []
+    reordered_expert_ids = []
+
+    tot_num_tokens = topk_ids.numel()
+    for e_id in range(num_experts):
+        tokens_for_expert = expert_to_tokens[e_id]
+        num_tokens = len(tokens_for_expert)
+
+        n_blocks = ((num_tokens + block_size - 1) // block_size)
+        # If not a multiple of block_size, pad up to the next multiple
+        padded_size = n_blocks * block_size
+
+        # Reorder all actual tokens for expert e_id
+        reordered_token_ids.extend(tokens_for_expert)
+        # reordered_expert_ids.extend([e_id]*num_tokens)
+        reordered_expert_ids.extend([e_id] * n_blocks)
+
+        # Pad with dummy token_id = -1 (or any sentinel), if needed
+        if padded_size > num_tokens:
+            pad_count = padded_size - num_tokens
+            reordered_token_ids.extend([tot_num_tokens] * pad_count)
+
+    token_length = len(reordered_token_ids)
+    expert_length = len(reordered_expert_ids)
+
+    sorted_token_ids[:token_length] = torch.tensor(reordered_token_ids, dtype=sorted_token_ids.dtype,
+                                                   device=sorted_token_ids.device)
+    expert_ids[:expert_length] = torch.tensor(reordered_expert_ids, dtype=expert_ids.dtype, device=expert_ids.device)
+
+    # Fill remainder with -1 if these arrays are bigger than total_length
+    if token_length < sorted_token_ids.numel():
+        sorted_token_ids[token_length:] = tot_num_tokens
+
+    num_tokens_post_pad.fill_(token_length)
+
+def torch_moe_align_block_size(
+    topk_ids: torch.Tensor, #[num_tkns, num_experts]
+    num_experts: int,
+    block_size: int,
+):
+    top_k = topk_ids.shape[1]
+    max_num_tokens_padded = topk_ids.numel() + num_experts * (block_size - 1)
+    sorted_ids = torch.empty((max_num_tokens_padded, ), dtype=torch.int32,
+                             device=topk_ids.device)
+    max_num_m_blocks = triton.cdiv(max_num_tokens_padded, block_size) 
+    expert_ids = torch.empty((max_num_m_blocks), dtype=torch.int32, device=topk_ids.device)
+    sorted_ids.fill_(topk_ids.numel())
+    num_tokens_post_pad = torch.empty((1), dtype=torch.int32, device=topk_ids.device)
+    _torch_moe_align_block_size(topk_ids, num_experts, top_k, block_size, sorted_ids, expert_ids, num_tokens_post_pad)
+
+    return sorted_ids, expert_ids, num_tokens_post_pad
+
+def input_helper(M: int, E: int, top_k: int):
+    values = torch.randn(M, E, dtype=torch.float16, device='cuda')
+
+    softmax_vals = torch.softmax(values, dim=1)
+    _, topk_ids = torch.topk(softmax_vals, k=top_k, dim=1) 
+
+    return topk_ids
+
+def triton_moe_align_block_size(
+    topk_ids: torch.Tensor,
+    block_size: int,
+    num_experts: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    max_num_tokens_padded = topk_ids.numel() + num_experts * (block_size - 1)
+    sorted_ids = torch.empty((max_num_tokens_padded, ),
+                                dtype=torch.int32,
+                                device=topk_ids.device)
+    sorted_ids.fill_(topk_ids.numel())
+    max_num_m_blocks = triton.cdiv(max_num_tokens_padded, block_size)
+    expert_ids = torch.empty((max_num_m_blocks, ),
+                                dtype=torch.int32,
+                                device=topk_ids.device)
+    num_tokens_post_pad = torch.empty((1),
+                                        dtype=torch.int32,
+                                        device=topk_ids.device)
+    
+    moe_align_block_size_triton(topk_ids,
+                                num_experts,
+                                block_size,
+                                sorted_ids,
+                                expert_ids,
+                                num_tokens_post_pad,
+                            )
+
+    return sorted_ids, expert_ids, num_tokens_post_pad
+
+@pytest.mark.parametrize("M, E, top_k, block_size", 
+    [(1, 2, 1, 16),
+    (4, 2, 1, 16),
+    (8, 2, 1, 16),
+    (16, 2, 1, 16),
+    (16, 8, 4, 16),
+    (16, 8, 4, 32),
+    (16, 8, 4, 128),
+    (16, 48, 4, 64),
+    (32, 224, 12, 128),
+    ],
+)
+def test_correctness(M: int, E: int, top_k: int, block_size: int):
+    topk_ids = input_helper(M, E, top_k)
+
+    tri_sorted_ids, tri_expert_ids, tri_num_tokens_post_pad = triton_moe_align_block_size(topk_ids, block_size, E)
+    torch_sorted_ids, torch_expert_ids, torch_num_tokens_post_pad = torch_moe_align_block_size(topk_ids, E, block_size)
+    
+    torch.eq(tri_sorted_ids, torch_sorted_ids)
+    torch.eq(tri_num_tokens_post_pad, torch_num_tokens_post_pad)
+    torch.eq(tri_expert_ids[:E], torch_expert_ids[:E])
+
+
