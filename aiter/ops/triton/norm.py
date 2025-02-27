5016d0d9d48c6ef00a56c3ab3474239ec7898edc
lucas-santos-amd <Lucas.Santos@amd.com>
2025-02-27 14:29:09 -0300

    Add Triton fused add LayerNorm Kernel (#151)

--- aiter/ops/triton/norm.py
+++ aiter/ops/triton/norm.py
@@ -6,29 +6,29 @@ import triton
 import triton.language as tl
 from typing import Optional
 
+
 @triton.jit
 def _layernorm_kernel(
-        # Pointers to matrices
-        x_ptr,
-        y_ptr,
-        w_ptr,
-        b_ptr,
-        mean_ptr,
-        rstd_ptr,
-        # The stride variables represent how much to increase the ptr by when
-        # moving by 1 element in a particular dimension. E.g. `x_row_stride` is
-        # how much to increase `x_ptr` by to get the element one row down.
-        x_row_stride,
-        y_row_stride,
-        # Matrix dimensions
-        n_rows,
-        n_cols,
-        # Epsilon to avoid division by zero
-        eps,
-        # Meta-parameters
-        BLOCK_SIZE: tl.constexpr):
+    # Pointers to matrices
+    x_ptr,
+    y_ptr,
+    w_ptr,
+    b_ptr,
+    # The stride variables represent how much to increase the ptr by when
+    # moving by 1 element in a particular dimension. E.g. `x_row_stride` is
+    # how much to increase `x_ptr` by to get the element one row down.
+    x_row_stride,
+    y_row_stride,
+    # Matrix dimensions
+    n_rows,
+    n_cols,
+    # Epsilon to avoid division by zero
+    eps,
+    # Meta-parameters
+    BLOCK_SIZE: tl.constexpr,
+):
     """
-    Note: this is Triton jited function and not meant to be called directly. Call fused_moe function
+    Note: this is Triton jited function and not meant to be called directly. Call layer_norm function
     below
 
     Applies Layer Normalization over a mini-batch of inputs.
@@ -52,12 +52,14 @@ def _layernorm_kernel(
     loop_num_l = loop_num
     for b in range(0, loop_num_l):
         col_offsets = b * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-        x_block = tl.load(x_ptr_start + col_offsets).to(tl.float32)  #Unmasked loads
+        x_block = tl.load(x_ptr_start + col_offsets).to(tl.float32)  # Unmasked loads
         _mean += x_block
 
     # For last iteration, do masked load
     col_offsets = loop_num_l * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    x_block = tl.load(x_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.).to(tl.float32)
+    x_block = tl.load(
+        x_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.0
+    ).to(tl.float32)
     _mean += x_block
     mean = tl.sum(_mean, axis=0) / n_cols
 
@@ -66,22 +68,20 @@ def _layernorm_kernel(
     loop_num_l = loop_num
     for b in range(0, loop_num_l):
         col_offsets = b * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-        x_block = tl.load(x_ptr_start + col_offsets).to(tl.float32)  #Unmasked loads
+        x_block = tl.load(x_ptr_start + col_offsets).to(tl.float32)  # Unmasked loads
         x_block = x_block - mean
         _var += x_block * x_block
 
-    #For last iteration, do masked load
+    # For last iteration, do masked load
     col_offsets = loop_num_l * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    x_block = tl.load(x_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.).to(tl.float32)
-    x_block = tl.where(col_offsets < n_cols, x_block - mean, 0.)
+    x_block = tl.load(
+        x_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.0
+    ).to(tl.float32)
+    x_block = tl.where(col_offsets < n_cols, x_block - mean, 0.0)
     _var += x_block * x_block
 
     var = tl.sum(_var, axis=0) / n_cols
     rstd = tl.rsqrt(var + eps)
-
-    # Write mean / rstd
-    tl.store(mean_ptr + row, mean)
-    tl.store(rstd_ptr + row, rstd)
 
     # Normalize and store
     loop_num_l = loop_num
@@ -104,34 +104,176 @@ def _layernorm_kernel(
     y_block = y_block * w_block + b_block
     tl.store(y_ptr_start + col_offsets, y_block, mask=mask)
 
-#TODO - Figure out x_bias
-def layer_norm(x: torch.Tensor,
-                weight: torch.Tensor,
-                bias: torch.Tensor,
-                eps: float = 1e-5,
-                x_bias: Optional[torch.Tensor] = None) -> torch.Tensor:
 
-    y = torch.empty_like(x)
-    M, N = x.shape
-    mean = torch.empty((M, ), dtype=torch.float32, device=x.device)
-    rstd = torch.empty((M, ), dtype=torch.float32, device=x.device)
+@triton.jit
+def _fused_add_layernorm_kernel(
+    # Pointers to matrices
+    x_ptr,
+    y_ptr,
+    res_in_ptr,
+    res_out_ptr,
+    w_ptr,
+    b_ptr,
+    # The stride variables represent how much to increase the ptr by when
+    # moving by 1 element in a particular dimension. E.g. `x_row_stride` is
+    # how much to increase `x_ptr` by to get the element one row down.
+    x_row_stride,
+    y_row_stride,
+    # Matrix dimensions
+    n_rows,
+    n_cols,
+    # Epsilon to avoid division by zero
+    eps,
+    # Meta-parameters
+    BLOCK_SIZE: tl.constexpr,
+):
+    """
+    Note: this is Triton jited function and not meant to be called directly. Call layernorm2d_fwd_with_add function
+    below
+
+    Performs an addition between two inputs and then applies Layer Normalization over
+    the addition result.
+
+    Key parameters:
+    - X: The input tensor to be normalized with shape (M, N).
+    - Y: The output tensor with the same shape as the input one.
+    - Res_in: The tensor to be added to the X tensor with shape (M, N).
+    - Res_out: The tensor in which the addition result will be stored with shape (M, N).
+    - W: The learnable weights tensor with shape (N, ).
+    - B: The learnable bias tensor with shape (N, ).
+    """
+    # Map the program id to the row of X and Y it should compute.
+    row = tl.program_id(0)
+    x_ptr_start = x_ptr + (row * x_row_stride)
+    y_ptr_start = y_ptr + (row * y_row_stride)
+    res_in_ptr_start = res_in_ptr + (row * x_row_stride)
+    res_out_ptr_start = res_out_ptr + (row * x_row_stride)
+
+    loop_num = tl.cdiv(n_cols, BLOCK_SIZE) - 1
+
+    # Calculate mean
+    mean = 0
+    _mean = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    loop_num_l = loop_num
+    for b in range(0, loop_num_l):
+        col_offsets = b * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        _x_block = tl.load(x_ptr_start + col_offsets)  # Unmasked loads
+        res_in_block = tl.load(res_in_ptr_start + col_offsets)
+        _x_block += res_in_block
+        tl.store(res_out_ptr_start + col_offsets, _x_block)  # Stores residual_out
+        _mean += _x_block.to(tl.float32)
+
+    # For last iteration, do masked load
+    col_offsets = loop_num_l * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    _x_block = tl.load(x_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.0)
+    res_in_block = tl.load(
+        res_in_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.0
+    )
+    _x_block += res_in_block
+    tl.store(
+        res_out_ptr_start + col_offsets, _x_block, mask=col_offsets < n_cols
+    )  # Stores residual_out
+    _mean += _x_block.to(tl.float32)
+    mean = tl.sum(_mean, axis=0) / n_cols
+
+    # Calculate variance
+    _var = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    loop_num_l = loop_num
+    for b in range(0, loop_num_l):
+        col_offsets = b * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        x_block = tl.load(res_out_ptr_start + col_offsets).to(
+            tl.float32
+        )  # Unmasked loads
+        x_block = x_block - mean
+        _var += x_block * x_block
+
+    # For last iteration, do masked load
+    col_offsets = loop_num_l * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    x_block = tl.load(
+        res_out_ptr_start + col_offsets, mask=col_offsets < n_cols, other=0.0
+    ).to(tl.float32)
+    x_block = tl.where(col_offsets < n_cols, x_block - mean, 0.0)
+    _var += x_block * x_block
+
+    var = tl.sum(_var, axis=0) / n_cols
+    rstd = tl.rsqrt(var + eps)
+
+    # Normalize and store
+    loop_num_l = loop_num
+    for b in range(0, loop_num_l):
+        col_offsets = b * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        w_block = tl.load(w_ptr + col_offsets)
+        b_block = tl.load(b_ptr + col_offsets)
+        x_block = tl.load(res_out_ptr_start + col_offsets).to(tl.float32)
+        y_block = (x_block - mean) * rstd
+        y_block = y_block * w_block + b_block
+        tl.store(y_ptr_start + col_offsets, y_block)
+
+    # For last iteration, do masked load and store
+    col_offsets = loop_num_l * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    mask = col_offsets < n_cols
+    w_block = tl.load(w_ptr + col_offsets, mask=mask, other=0.0)
+    b_block = tl.load(b_ptr + col_offsets, mask=mask, other=0.0)
+    x_block = tl.load(res_out_ptr_start + col_offsets, mask=mask, other=0.0).to(
+        tl.float32
+    )
+    y_block = (x_block - mean) * rstd
+    y_block = y_block * w_block + b_block
+    tl.store(y_ptr_start + col_offsets, y_block, mask=mask)
+
+
+def layer_norm(
+    input: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    eps: float = 1e-5,
+    x_bias: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+
+    out = torch.empty_like(input)
+    M, N = input.shape
 
     # Less than 64KB per feature: enqueue fused kernel
-    MAX_FUSED_SIZE = 65536 // x.element_size()
+    MAX_FUSED_SIZE = 65536 // input.element_size()
     BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
 
-    _layernorm_kernel[(M, )](
-        x,
-        y,
+    _layernorm_kernel[(M,)](
+        input, out, weight, bias, input.stride(0), out.stride(0), M, N, eps, BLOCK_SIZE
+    )
+
+    return out
+
+
+def layernorm2d_fwd_with_add(
+    out: torch.Tensor,
+    input: torch.Tensor,
+    residual_in: torch.Tensor,
+    residual_out: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    epsilon: float,
+    x_bias: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+
+    M, N = input.shape
+
+    # Less than 64KB per feature: enqueue fused kernel
+    MAX_FUSED_SIZE = 65536 // input.element_size()
+    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+
+    _fused_add_layernorm_kernel[(M,)](
+        input,
+        out,
+        residual_in,
+        residual_out,
         weight,
         bias,
-        mean,
-        rstd,
-        x.stride(0),
-        y.stride(0),
+        input.stride(0),
+        out.stride(0),
         M,
         N,
-        eps,
-        BLOCK_SIZE)
+        epsilon,
+        BLOCK_SIZE,
+    )
 
-    return y
+    return
--- op_tests/triton/test_layernorm.py
+++ op_tests/triton/test_layernorm.py
@@ -1,11 +1,49 @@
 import triton
 import torch
+import torch.nn.functional as F
 import triton.language as tl
 import pytest
 from aiter.ops.triton.norm import layer_norm
+from aiter.ops.triton.norm import layernorm2d_fwd_with_add
+
+
+def run_torch(input, weight, bias, eps, residual=None, x_bias=None):
+    if residual is None:
+        residual_out = None
+        output = F.layer_norm(
+            input=input,
+            normalized_shape=(input.shape[-1],),
+            weight=weight,
+            bias=bias,
+            eps=eps,
+        )
+    else:
+        residual_out = input + residual
+        output = F.layer_norm(
+            input=residual_out,
+            normalized_shape=(input.shape[-1],),
+            weight=weight,
+            bias=bias,
+            eps=eps,
+        )
+    return output, residual_out
+
+
+def run_triton(input, weight, bias, eps, residual=None, x_bias=None):
+    if residual is None:
+        residual_out = None
+        output = layer_norm(input, weight, bias, eps, x_bias)
+    else:
+        residual_out = torch.empty_like(input)
+        output = torch.empty_like(input)
+        layernorm2d_fwd_with_add(
+            output, input, residual, residual_out, weight, bias, eps, x_bias
+        )
+    return output, residual_out
 
 
 # pytest
+@pytest.mark.parametrize("dtype_str", ["fp32", "fp16", "bf16"])
 @pytest.mark.parametrize(
     "M, N",
     [
@@ -22,16 +60,73 @@ from aiter.ops.triton.norm import layer_norm
         (1, 89999),
     ],
 )
-def test_layernorm(M, N, eps=1e-5):
-
+def test_layernorm(M, N, dtype_str, eps=1e-5):
+    arg_to_torch_dtype = {
+        "fp16": torch.float16,
+        "bf16": torch.bfloat16,
+        "fp32": torch.float32,
+    }
+    dtype = arg_to_torch_dtype[dtype_str]
     torch.manual_seed(0)
-    x = torch.randn(M, N, device="cuda")
+    x = torch.randn(M, N, device="cuda", dtype=dtype)
     w_shape = (N,)
-    w = torch.rand(w_shape, device="cuda")
-    b = torch.rand(w_shape, device="cuda")
+    b = torch.rand(w_shape, device="cuda", dtype=dtype)
+    w = torch.rand(w_shape, device="cuda", dtype=dtype)
 
     # forward pass
-    y_triton = layer_norm(x, w, b, eps)
-    y_ref = torch.nn.functional.layer_norm(x, w_shape, w, b, eps)
+    y_torch, *_ = run_torch(x, w, b, eps)
+    y_triton, *_ = run_triton(x, w, b, eps)
 
-    triton.testing.assert_close(y_triton, y_ref, atol=1e-06, rtol=1e-05)
+    if dtype in (torch.float16, torch.bfloat16):
+        atol, rtol = 1e-2, 1e-2
+    else:
+        # float32 typically can be tighter
+        atol, rtol = 1e-5, 1e-5
+
+    triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
+
+
+# pytest
+@pytest.mark.parametrize("dtype_str", ["fp32", "fp16", "bf16"])
+@pytest.mark.parametrize(
+    "M, N",
+    [
+        (1823, 781),
+        (2, 128),
+        (1, 4),
+        (128, 2),
+        (1, 128),
+        (8192, 8192),
+        (4096, 8192),
+        (359, 1),
+        (1, 359),
+        (1, 131072),
+        (1, 89999),
+    ],
+)
+def test_fused_add_layernorm(M, N, dtype_str, eps=1e-5):
+    arg_to_torch_dtype = {
+        "fp16": torch.float16,
+        "bf16": torch.bfloat16,
+        "fp32": torch.float32,
+    }
+    dtype = arg_to_torch_dtype[dtype_str]
+    torch.manual_seed(0)
+    x = torch.randn(M, N, device="cuda", dtype=dtype)
+    res = torch.randn(M, N, device="cuda", dtype=dtype)
+    w_shape = (N,)
+    w = torch.rand(w_shape, device="cuda", dtype=dtype)
+    b = torch.rand(w_shape, device="cuda", dtype=dtype)
+
+    # forward pass
+    y_torch, res_torch, *_ = run_torch(x, w, b, eps, residual=res)
+    y_triton, res_triton, *_ = run_triton(x, w, b, eps, residual=res)
+
+    if dtype in (torch.float16, torch.bfloat16):
+        atol, rtol = 1e-2, 1e-2
+    else:
+        # float32 typically can be tighter
+        atol, rtol = 1e-5, 1e-5
+
+    triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
+    triton.testing.assert_close(res_triton, res_torch, atol=atol, rtol=rtol)
