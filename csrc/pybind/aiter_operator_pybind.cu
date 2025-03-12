fc828c8f63f9250f6efbe38840f2257d3283588f
TennyWang1223 <Tenny.Wang@amd.com>
2025-03-12 16:22:18 +0800

    [feat]: Added elementwise inp interface (#189)
    Signed-off-by: TennyWang <tennwang@amd.com>
    Co-authored-by: TennyWang <tennwang@amd.com>

--- csrc/pybind/aiter_operator_pybind.cu
+++ csrc/pybind/aiter_operator_pybind.cu
@@ -8,6 +8,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     m.def("mul", &aiter_mul, "apply for mul with transpose and broadcast.");
     m.def("sub", &aiter_sub, "apply for sub with transpose and broadcast.");
     m.def("div", &aiter_div, "apply for div with transpose and broadcast.");
+    m.def("add_", &aiter_add_, "apply for add_ with transpose and broadcast.");
+    m.def("mul_", &aiter_mul_, "apply for mul_ with transpose and broadcast.");
+    m.def("sub_", &aiter_sub_, "apply for sub_ with transpose and broadcast.");
+    m.def("div_", &aiter_div_, "apply for div_ with transpose and broadcast.");
     m.def("sigmoid", &aiter_sigmoid, "apply for sigmoid.");
     m.def("tanh", &aiter_tanh, "apply for tanh.");
 }
